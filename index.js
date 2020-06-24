6a19d3ba8ea6585377fa730aeb7316d874aadc37
adaptiff <generatiff.com@gmail.com>
2020-06-24 15:11:51 +0400

    add missing func

--- index.js
+++ index.js
@@ -162,3 +162,22 @@ function downloadFromDataURL(filename, dataUrl) {
 
   document.body.removeChild(element);
 }
+
+// from Abhinav's answer at  https://stackoverflow.com/questions/37135417/download-canvas-as-png-in-fabric-js-giving-network-error/
+export const dataURLtoBlob = function (dataurl) {
+  var parts = dataurl.split(","),
+    mime = parts[0].match(/:(.*?);/)[1];
+  if (parts[0].indexOf("base64") !== -1) {
+    var bstr = atob(parts[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new Blob([u8arr], { type: mime });
+  } else {
+    var raw = decodeURIComponent(parts[1]);
+    return new Blob([raw], { type: mime });
+  }
+};
